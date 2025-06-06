ad8add162e9118965e9d3163d30c3807bc5b2a14
정찬영 <jcy0342@gmail.com>
2025-06-06 17:09:23 +0900

    목표 상세 페이지 퍼블리싱 시작

---
+++ src/app/(dashboard)/goals/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function GoalDetailPage() {
+  return <div>GoalDetailPage</div>;
+}
+
+//todo: 상세 페이지 구현
--- src/app/(dashboard)/goals/page.tsx
+++ src/app/(dashboard)/goals/page.tsx
@@ -9,6 +9,7 @@ import {
   CheckCircle2,
 } from "lucide-react";
 import { useState } from "react";
+import Link from "next/link";
 
 enum Period {
   WEEKLY = "weekly",
@@ -296,7 +297,9 @@ function GoalGrid() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {goals.map((goal) => (
-        <GoalCard key={goal.id} goal={goal} />
+        <Link key={goal.id} href={`/goals/${goal.id}`}>
+          <GoalCard goal={goal} />
+        </Link>
       ))}
     </div>
   );
