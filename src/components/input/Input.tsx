96dcc60fc4aa53060686edd126e65f64cfeeae67
SSY1203 <tlstjsdud566@naver.com>
2025-06-08 02:36:45 +0900

    Input 컴포넌트 구현

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,73 @@
+import React, { ChangeEvent } from "react";
+
+interface InputProps {
+  type?: "text" | "password" | "email" | "textarea" | "date";
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  label: string;
+  labelVisible?: boolean;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  readOnly?: boolean;
+  error?: { message: string } | null;
+}
+
+const Input = ({
+  type = "text",
+  value,
+  onChange,
+  label,
+  labelVisible = true,
+  placeholder = "",
+  required = false,
+  disabled = false,
+  readOnly = false,
+  error: errorProp = null,
+}: InputProps) => {
+  const isTextarea = type === "textarea";
+
+  return (
+    <fieldset className="fieldset">
+      <label
+        className={`fieldset-legend justify-start gap-1 text-[0.9rem]  ${
+          !labelVisible ? "sr-only" : ""
+        }`}
+      >
+        {label}
+        {required ? <span className="text-red-500">*</span> : null}
+      </label>
+      {isTextarea ? (
+        <textarea
+          className={`textarea w-full`}
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) =>
+            onChange && onChange(e as ChangeEvent<HTMLTextAreaElement>)
+          }
+          disabled={disabled}
+          readOnly={readOnly}
+          aria-label={label}
+          aria-required={!readOnly}
+        />
+      ) : (
+        <input
+          type={type}
+          className={`input w-full `}
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) =>
+            onChange && onChange(e as ChangeEvent<HTMLInputElement>)
+          }
+          disabled={disabled}
+          readOnly={readOnly}
+          aria-label={label}
+          aria-required={!readOnly}
+        />
+      )}
+      {errorProp && <p className="label validator-hint">{errorProp.message}</p>}
+    </fieldset>
+  );
+};
+
+export default Input;
