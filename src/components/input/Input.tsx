6e8fa1898f78d3c96cb6b4bfe9c882f44e11a1c8
SSY1203 <tlstjsdud566@naver.com>
2025-06-08 13:25:42 +0900

    Date Picker 기능 추가

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { format, getDay } from "date-fns";
 import React, { ChangeEvent } from "react";
+import { DayPicker } from "react-day-picker";
 
 interface InputProps {
   type?: "text" | "password" | "email" | "textarea" | "date";
-  value?: string;
-  onChange?: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  value?: string | Date | null;
+  onChange?: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | Date
+  ) => void;
   label: string;
   labelVisible?: boolean;
   placeholder?: string;
@@ -26,6 +32,15 @@ const Input = ({
   error: errorProp = null,
 }: InputProps) => {
   const isTextarea = type === "textarea";
+  const isDate = type === "date";
+
+  const calendarFormatter = {
+    formatCaption: (date: Date) => format(date, "yyyy년 MM월"),
+    formatWeekdayName: (weekDay: Date) => {
+      const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+      return weekdays[getDay(weekDay)];
+    },
+  };
 
   return (
     <fieldset className="fieldset">
@@ -41,7 +56,7 @@ const Input = ({
         <textarea
           className={`textarea w-full`}
           placeholder={placeholder}
-          value={value}
+          value={value as string}
           onChange={(e) =>
             onChange && onChange(e as ChangeEvent<HTMLTextAreaElement>)
           }
@@ -50,12 +65,43 @@ const Input = ({
           aria-label={label}
           aria-required={!readOnly}
         />
+      ) : isDate ? (
+        <>
+          <button
+            popoverTarget={`rdp-popover-${label}`}
+            className="input input-border w-full"
+            style={{ anchorName: `--rdp-${label}` } as React.CSSProperties}
+          >
+            {value ? (
+              format(value as Date, "yyyy-MM-dd")
+            ) : (
+              <span className="text-gray-400">
+                {`${label}을 선택해 주세요`}
+              </span>
+            )}
+          </button>
+          <div
+            popover="auto"
+            id={`rdp-popover-${label}`}
+            className="dropdown"
+            style={{ positionAnchor: `--rdp-${label}` } as React.CSSProperties}
+          >
+            <DayPicker
+              className="react-day-picker"
+              mode={"single"}
+              required
+              selected={value as Date}
+              onSelect={onChange && ((date) => onChange(date as Date))}
+              formatters={calendarFormatter}
+            />
+          </div>
+        </>
       ) : (
         <input
           type={type}
           className={`input w-full`}
           placeholder={placeholder}
-          value={value}
+          value={value as string}
           onChange={(e) =>
             onChange && onChange(e as ChangeEvent<HTMLInputElement>)
           }
@@ -65,6 +111,7 @@ const Input = ({
           aria-required={!readOnly}
         />
       )}
+
       {errorProp && <p className="label validator-hint">{errorProp.message}</p>}
     </fieldset>
   );
